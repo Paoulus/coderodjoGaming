6e5f919369f5311ddc6bb1499bfd04eb2f7cd555
PCH <paolo.chiste-2@studenti.unitn.it>
2018-04-26 23:25:23 +0200

    Added map and tileset. Changed import code

--- mushroomsRedux/js/game.js
+++ mushroomsRedux/js/game.js
@@ -7,13 +7,16 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'gameDiv',
 
 function preload(){
 	game.load.tilemap('mappa','mappa-esportata.json',null,Phaser.Tilemap.TILED_JSON);
-	game.load.image('tileset','tiles-1');
+	game.load.image('tileset','tiles-1.png');
 }
 	
 var mappa;
 function create(){
 	mappa = game.add.tilemap('mappa');
-	mappa.addTilesetImage('tileset');
+	mappa.addTilesetImage('tileset','tileset');
+	
+	var layer = mappa.createLayer('Livello tile 1');
+	layer.resizeWorld();
 }
 
 function update(){
