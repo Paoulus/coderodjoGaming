5867e3acf8b248522bd219b9c48fc05065c062db
PCH <paolo.chiste-2@studenti.unitn.it>
2018-05-10 20:24:08 +0200

    Added robot walk  animations

--- mushroomsRedux/js/game.js
+++ mushroomsRedux/js/game.js
@@ -1,35 +1,58 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'gameDiv', 
-	{ 
-		preload: preload, 
-		create: create, 
-		update: update 
-	});
+var game = new Phaser.Game(800,480,Phaser.AUTO,"gameDiv",
 
-function preload(){
-	game.load.tilemap('mappa','mappa-esportata.json',null,Phaser.Tilemap.TILED_JSON);
-	game.load.image('tileset','tiles-1.png');
-}
-	
-var mappa;
-function create(){
-	mappa = game.add.tilemap('mappa');
-	mappa.addTilesetImage('tileset','tileset');
-	
-	var layer = mappa.createLayer('Livello tile 1');
-	layer.resizeWorld();
+{
+    preload: preload,
+    create: create,
+    update: update
+
+});
+var map;
+var layer;
+
+
+function preload()
+{
+    game.load.tilemap('mappa','tilemap600x20.json',null,Phaser.Tilemap.TILED_JSON);
+    game.load.image('tileset','tiles-1.png');
+
+    game.load.atlas('robot','atlas_robot_basicPackaging.png','atlas_robot_basicPackaging.json');
 }
 
+var robot;
+function create()
+{
+    /*
+    map = game.add.tilemap("mappa");
+    map.addTilesetImage("tileset");
+    layer = map.createLayer("Livello tile 1");
+    layer.resizeWorld();
+    */
+
+    robot = game.add.sprite(50,50,'robot','Idle (2).png');
+    game.physics.enable(robot,Phaser.Physics.ARCADE,true);
+    robot.anchor.setTo(0.5,0.5);
+
+    robot.animations.add('run',['Run (1).png','Run (2).png'],4,true,false);
+    robot.animations.add('still',['Idle (1).png'],1,false,false);
+}
+
+var robotVelociy = 150;
 function update(){
-
+    if(game.input.keyboard.isDown(Phaser.Keyboard.D))
+    {
+        robot.body.velocity.setTo(robotVelociy,0);
+        robot.animations.play('run');
+        robot.scale.setTo(1,1);
+    }
+    else if(game.input.keyboard.isDown(Phaser.Keyboard.A))
+    {
+        robot.body.velocity.setTo(-robotVelociy,0);
+        robot.animations.play('run');
+        robot.scale.setTo(-1,1);
+    }
+    else{
+        robot.body.velocity.setTo(0,0);
+        robot.animations.stop('run',true);
+        robot.animations.play('still');
+    }
 }
-
-
-
-
-	
-
-
-
-	
-
-
