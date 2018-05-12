f482ee4ac07cbdd870ed19e6740871499b20b42a
PCH <paolo.chiste-2@studenti.unitn.it>
2018-05-12 20:24:05 +0200

    Enabled collisions between player and the map

--- mushroomsRedux/js/game.js
+++ mushroomsRedux/js/game.js
@@ -7,7 +7,7 @@ var game = new Phaser.Game(800,480,Phaser.AUTO,"gameDiv",
 
 });
 var map;
-var layer;
+var mapLayer;
 
 
 function preload()
@@ -21,17 +21,28 @@ function preload()
 var robot;
 function create()
 {
-    
+	//attiva fisica 
+	game.physics.startSystem(Phaser.Physics.ARCADE);
+	
+    //impostazione gravità locale
+	game.physics.arcade.gravity.y = 2984;
+	
+	//impostazione mappa
     map = game.add.tilemap("mappa");
     map.addTilesetImage("tileset");
-    layer = map.createLayer("Livello tile 1");
-    layer.resizeWorld();
+    mapLayer = map.createLayer("Livello tile 1");
+    mapLayer.resizeWorld();
+	
+	map.setCollisionBetween(0,50);
     
-
+	//impostazione proprietà robot
     robot = game.add.sprite(50,50,'robot','Idle (2).png');
+	robot.scale.setTo(0.27,0.27);
     game.physics.enable(robot,Phaser.Physics.ARCADE,true);
     robot.anchor.setTo(0.5,0.5);
-
+	robot.body.colliderWorldBounds = true;
+	
+	//animazioni 
     robot.animations.add('run',['Run (1).png','Run (2).png'],4,true,false);
     robot.animations.add('still',['Idle (1).png'],1,false,false);
 }
@@ -39,17 +50,19 @@ function create()
 var robotVelocity = 150;
 function update()
 {
+	game.physics.arcade.collide(robot,mapLayer);
+	
     if(game.input.keyboard.isDown(Phaser.Keyboard.D))
     {
         robot.body.velocity.setTo(robotVelocity,0);
         robot.animations.play('run');
-        robot.scale.setTo(1,1);
+        robot.scale.setTo(0.27,0.27);
     }
     else if(game.input.keyboard.isDown(Phaser.Keyboard.A))
     {
         robot.body.velocity.setTo(-robotVelocity,0);
         robot.animations.play('run');
-        robot.scale.setTo(-1,1);
+        robot.scale.setTo(-0.27,0.27);
     }
     else{
         robot.body.velocity.setTo(0,0);
