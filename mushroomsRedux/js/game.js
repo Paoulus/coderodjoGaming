dd44f3b79f79350e59241cdf787f76d9e806201e
Paolo <paolo.chiste-2@studenti.unitn.it>
2018-07-12 12:13:05 +0200

    Fixed all broken calls to use Phaser3 API.

--- mushroomsRedux/js/game.js
+++ mushroomsRedux/js/game.js
@@ -18,7 +18,7 @@ var config = {
 var game = new Phaser.Game(config);
 
 var map;
-var mapLayer;
+var mapCollisionLayer;
 
 
 function preload()
@@ -35,12 +35,20 @@ function preload()
 var robot;
 var checkpoints;
 var enemies;
+var keyA;
+var keyD;
+var keySpacebar;
 function create()
 {	
+    //assegnazione tasti 
+    keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+    keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+    keySpacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+
 	//impostazione mappa
     map = this.add.tilemap("mappa");
     var tileset = map.addTilesetImage('tiles-1');
-    mapLayer = map.createStaticLayer("Livello tile 1",tileset);
+    mapCollisionLayer = map.createStaticLayer("Livello tile 1",tileset);
 	
     //include ALL the tiles
 	map.setCollisionBetween(0,67);
@@ -51,6 +59,7 @@ function create()
     robot.scaleY = 0.3;
     robot.anchorX = 0.5;
     robot.anchorY = 0.5;
+    robot.body.setCollideWorldBounds(true);
 
     this.anims.create({
         key: "run",
@@ -60,6 +69,14 @@ function create()
     this.anims.create({
         key: "flagUp",
         frames: this.anims.generateFrameNumbers('flag', { start: 0, end: 4, first: 0 })
+    });
+    this.anims.create({
+        key: "still",
+        frames: this.anims.generateFrameNames('robot', { prefix:'Idle (',start:1,end:8,suffix:').png' })
+    });
+    this.anims.create({
+        key: "jump",
+        frames: this.anims.generateFrameNames('robot', { prefix:'Jump (',start:1,end:8,suffix:').png' })
     });
 
     //gruppi oggetti
@@ -80,40 +97,42 @@ function create()
     //                      nome del gruppo in cui inserire questi oggetti (checkpoints in questo caso)
 
     map.createFromObjects('Enemies',75,'slime',1,true,false,enemies);
+
+    this.physics.add.collider(robot, mapCollisionLayer);
 }
 
 var robotVelocity = 150;
 var jumpTime = 0;
 function update()
 {
-    if((Phaser.Input.Keyboard.KeyCodes.SPACEBAR))
+    if(keySpacebar.isDown)
     {
         if(robot.body.onFloor() && this.time.now - jumpTime > 90)
         {
             robot.body.velocity.y = -120;
-            jumpTime = this.time.now;    
+            jumpTime = this.time.now;
         }
     }
 
-    if((Phaser.Input.Keyboard.KeyCodes.D))
+    if(keyD.isDown)
     {
         robot.body.velocity.x = robotVelocity;
-        robot.play('run');
+        robot.play('run',true);
         robot.scaleX = 0.3;
         robot.scaleY = 0.3;
     }
-    else if((Phaser.Input.Keyboard.KeyCodes.A))
+    else if(keyA.isDown)
     {
         robot.body.velocity.x = -robotVelocity;
-        robot.play('run');
-        robot.scaleX = -0.3
+        robot.play('run',true);
+        robot.scaleX = -0.3;
+        robot.scaleY = 0.3;
     }
     else{
 		//non azzerare la velocità ad ogni frame, altrimenti la gravità non funziona correttamente
-        //robot.body.velocity.setTo(0,0);
 		robot.body.velocity.x = 0;
-        robot.stop('run',true);
-        robot.play('still');
+
+        robot.play('still',true);
     }
 }
 
